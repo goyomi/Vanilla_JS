5ac4a98567c43663754e6f22cb9c66612db33cbd
goyomi <ghdl1004797@gmail.com>
2023-11-26 22:47:05 +0900

    Feat: 가위바위보 게임 로직 추가

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,28 +1,65 @@
+// 가위바위보 게임 함수 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function rspPlayer(userRsp) {
-  const rsp = ["가위", "바위", "보"];
-  const computerRsp = rsp[getRandomInteger(0, 2)];
+// 이미지 변경관련 변수와 함수 
+const randomImg = document.getElementById("randomImg");
+const imgs = ['./Asset/icon_scissors.svg', './Asset/icon_rock.svg', './Asset/icon_paper.svg'];
+let imgChangeTimer;
+let computerChoiceIndex;
+
+function startImgChange() {
+  imgChangeTimer = setInterval(ChangeImgs, 100);
+}
+function stopImgChange() {
+  clearInterval(imgChangeTimer);
+}
+function ChangeImgs() {
+  computerChoiceIndex = getRandomInteger(0, 2);
+  randomImg.src = imgs[computerChoiceIndex];
+}
+
+// 사용자 선택 및 게임 결과 처리 함수 
+let userRsp = null;
+
+function rspPlayer() {
+  const rsp = ["scissors", "rock", "paper"];
+  const computerRsp = rsp[computerChoiceIndex];
 
   const winValue = {
-    가위: "보",
-    바위: "가위",
-    보: "바위",
+    scissors: "rock",
+    rock: "scissors",
+    paper: "paper",
   };
 
   console.log("사용자가 선택한 것", userRsp);
   console.log("컴퓨터가 선택한 것", computerRsp);
 
-  const result =
+  const resultMessage =
     userRsp === computerRsp
-      ? "무승부"
+      ? "무승부입니다."
       : winValue[userRsp] === computerRsp
-        ? "승리"
-        : "패배";
+        ? "이겼습니다."
+        : "패배하셨습니다.";
 
-  if (result === "무승부") alert("무승부입니다.");
-  if (result === "승리") alert("이겼습니다.");
-  if (result === "패배") alert("패배하셨습니다.");
+  alert(resultMessage);
+  startImgChange();
 }
+
+// 이벤트 리스너
+document.querySelector('#userClicked').addEventListener("click", function (event) {
+  stopImgChange();
+
+  let clickElement = event.target;
+  if (clickElement.tagName === 'IMG') {
+    clickElement = clickElement.parentElement;
+  }
+
+  userRsp = clickElement.id;
+  rspPlayer();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  startImgChange();
+});
